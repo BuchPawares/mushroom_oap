4ce6149a0474e621c0dc6244a499efd34a97e72f
Pawares Jaithum <125111002+BuchPawares@users.noreply.github.com>
2025-06-03 14:07:31 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
 import os
